8eda2069752cff06ea8be95937854a6b43fbdaef
MiniOcean404 <1037306928@qq.com>
2023-05-19 01:49:39 +0800

    feat: 整理

--- lib/utils/system/index.dart
+++ lib/utils/system/index.dart
@@ -7,8 +7,12 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_video/utils/plugin/index.dart';
+import 'package:flutter/foundation.dart' as foundation;
 
 part 'color.dart';
 part 'get_layout.dart';
 part 'immerse.dart';
 part 'water_ripple.dart';
+part 'compile_mode.dart';
+part 'get_env.dart';
+part 'get_local_ip.dart';
